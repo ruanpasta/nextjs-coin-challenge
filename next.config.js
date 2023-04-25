dcac36aede45f90a4a980830ccb843558e8591db
Ruan Pasta <ruanpasta@gmail.com>
2023-04-25 02:25:25 -0300

    fix: update next api cross origin

--- next.config.js
+++ next.config.js
@@ -4,6 +4,14 @@ const nextConfig = {
   images: {
     domains: ['s3.eu-central-1.amazonaws.com'],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://coin-api.ruanpasta.com/api/:path*',
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig

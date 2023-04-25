e204bc0688c004d188332a10faffeaaf581884d8
Ruan Pasta <ruanpasta@gmail.com>
2023-04-25 03:01:35 -0300

    fix: update next api cross origin

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:3000/api/:path*',
+        destination: 'https://coin-api.ruanpasta.com/api/:path*',
       },
     ]
   },

b297d81db2115473518cb3429fc401c486d222f3
Ruan Pasta <ruanpasta@gmail.com>
2023-04-25 02:58:59 -0300

    fix: update next api cross origin

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,11 +1,15 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from 'next'
 
 export const timer = (time: number) =>
-    new Promise((resolve) => setTimeout(resolve, time))
+  new Promise((resolve) => setTimeout(resolve, time))
 
-export const cors = (req: NextApiRequest, res: NextApiResponse, next: () => void) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  next();
+export const cors = (
+  _: NextApiRequest,
+  res: NextApiResponse,
+  next: () => void
+) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS')
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+  next()
 }
